80c9910231c85bb40b99fc663c199b75be570b36
theohorlaville <theo.horlaville76@orange.fr>
2022-03-24 14:47:55 +0100

    [Wanted] Update : rules + front

--- src/script.js
+++ src/script.js
@@ -38,3 +38,46 @@ function getValueDe(n) {
 }
 
 getValueDe(n);
+
+window.addEventListener("load", function () {
+
+    const difficulty = document.querySelector(".gamemode")
+    var jeu = document.querySelector("#jeu")
+    var menu = document.querySelector("#menu")
+    var footer = document.querySelector("footer")
+
+    var temps = 120
+    var score = 0;
+    var bool = false
+
+    display_score();
+
+    difficulty.addEventListener("click", function () {
+        menu.style.width = 0 + "%"
+        jeu.style.width = 100 + "%"
+        bool = true
+        setTimeout(function () {
+            footer.style.display = "none";
+        }, 150)
+    })
+
+    setInterval(display_time, 1000)
+
+
+    function display_time() {
+        if (bool) {
+            var timer = document.querySelector("#timer")
+            temps--
+            if (temps >= 0) {
+                console.log("test")
+                timer.innerHTML = "temps restant : " + temps
+            }
+        }
+    }
+
+    function display_score() {
+        var scoreBoard = document.querySelector("#score")
+        scoreBoard.innerHTML = "score : " + score
+    }
+
+})

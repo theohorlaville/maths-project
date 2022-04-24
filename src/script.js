797077da62b37f074b8f2b610f7bc27f0243c8ed
Sorimoutou <sori.sully@gmail.com>
2022-04-24 17:38:19 +0200

    [draw_image] fixed

--- src/script.js
+++ src/script.js
@@ -45,7 +45,20 @@ window.addEventListener("load", function () {
     display_score()
     display_wanted()
     setInterval(display_time, 1000)
-    setInterval(draw_image, 1000)
+
+      /* ------------- random positon image ------------------ */
+      function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+    
+    // random departure position
+    let image_position_x = getRandomArbitrary(1,canvas.width-1)
+    let image_position_y = getRandomArbitrary(1,canvas.height-1)
+    let image_direction_x = getRandomArbitrary(5,15)
+    let image_direction_y = getRandomArbitrary(0,10)
+    setInterval(draw_image, 500) 
+
+    // move speed : the more the second number is low the more the images are fast
     //draw_image()
 
 
@@ -69,26 +82,36 @@ window.addEventListener("load", function () {
         wanted.src = "./assets/" + tab_wanted[0] + ".png"
     }
 
-    let image_position_x = 0.05
-    let image_direction = 10
+  
+
+    /* ------------- Draw function  ------------------ */
+
 
     function draw_image() {
         base_image = new Image();
         base_image.src = './assets/1.png';
 
-        if(image_position_x > 0  && image_position_x < canvas.width) {
+        // random number for X direction changing each call
+        // bouncing on canvas edges 
+
+        if(image_position_x >= 0  && image_position_x <= canvas.width && image_position_y >= 0 && image_position_y <= canvas.height) {
             
-            image_position_x += image_direction
-            console.log(image_direction)
+            image_position_x += image_direction_x*Math.random()
+            image_position_y += image_direction_y* Math.random()
+            console.log(image_position_y)
         }
         else {
-            image_direction *= -1
-            image_position_x += image_direction
+            image_direction_x *= -1
+            image_direction_y *= -1
+            image_position_x += image_direction_x*Math.random()
+            image_position_y += image_direction_y*Math.random()
             console.log('cc')
+            console.log(image_position_y)
+
         }
 
         base_image.onload = function () {
-            context.drawImage(base_image, image_position_x, 0, 20, 20);
+            context.drawImage(base_image, image_position_x, image_position_y, 20, 20);
         }
         context.clearRect(0, 0, canvas.width, canvas.height);
        // requestAnimationFrame(draw_image);
